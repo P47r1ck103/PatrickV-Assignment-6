59cecae9ea5f36f4f27e106c42cd1310552a06b8
p47r1ck103 <p47r1ck.v@gmail.com>
2024-12-16 13:11:30 -0800

    update.

--- src/com/coderscampus/A6/SalesData.java
+++ src/com/coderscampus/A6/SalesData.java
@@ -1,5 +1,25 @@
 package com.coderscampus.A6;
 
 public class SalesData {
+	private String model;
+	private int sales;
+	private LocalDate date;
 
+	public SalesData(String model, int sales, LocalDate date) {
+		this.model = model;
+		this.sales = sales;
+		this.date = date;
+	}
+
+	public String getModel() {
+		return model;
+	}
+
+	public int getSales() {
+		return sales;
+	}
+
+	public LocalDate getDate() {
+		return date;
+	}
 }
--- src/com/coderscampus/A6/SalesDataReader.java
+++ src/com/coderscampus/A6/SalesDataReader.java
@@ -1,5 +1,30 @@
 package com.coderscampus.A6;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
 
 public class SalesDataReader {
+	public List<SalesData> readData(String filePath) throws IOException{
+		List<SalesData> salesDataList = new ArrayList<>();
+		BufferedReader reader = new BufferedReader(new FileReader(filePath));
+		String line;
+		
+		reader.readLine();
+			while ((line = reader.readLine()) != null) {
+				String[] parts = line.split(",");
+			if (parts.length == 3) {
+				String model = parts[0];
+				int sales = integer.parseInt(parts[1]);
+				LocalDate date = LocalDate.parse(oarts[2]), DateTimeFormatter.ofPatterm("yyyy,MM,dd"));
+					salesDataList.add(new SalesData(model, slaes, date));
+			}
+		}
+			reader.close();
+			return salesDataList;
+	}
 
 }
