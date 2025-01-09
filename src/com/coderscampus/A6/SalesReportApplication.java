721ae7606e087980938d00e47f4ed5bf704562db
p47r1ck103 <p47r1ck.v@gmail.com>
2025-01-09 13:42:03 -0800

    final update

--- src/com/coderscampus/A6/SalesReportApplication.java
+++ src/com/coderscampus/A6/SalesReportApplication.java
@@ -60,14 +60,14 @@ public class SalesReportApplication {
 			}
 			if (bestMonth != null) {
 				System.out
-						.println("The best monthly sales was: " + bestMonth.getValue());
+						.println("The best month was: " + bestMonth.getKey() +" with sales: " + bestMonth.getValue());
 			} else {
 				System.out.println("No sales data available for the best month.");
 			}
 
 			if (worstMonth != null) {
 				System.out.println(
-						"The worst monthly sales was: " + worstMonth.getValue());
+						"The worst month was: " + worstMonth.getKey() + " with sales: " + worstMonth.getValue() );
 
 			} else {
 				System.out.println(" no sales data available for the worst month.");
